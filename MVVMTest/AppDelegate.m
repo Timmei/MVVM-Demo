03e7954a8f1c64ddd181dca7ff4873611b4386bd
meichaoliang <meichaoliang@youbanban.com>
2018-04-24 22:30:55 +0800

    helo

--- MVVMTest/AppDelegate.m
+++ MVVMTest/AppDelegate.m
@@ -14,6 +14,9 @@
 
 @implementation AppDelegate
 
+- (void)set{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"www.baidu.com"]];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
